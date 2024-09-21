84063aa02ac4820cfd97aa3e8fc261a751fa9670
FudgyDRS <yetimonsters@gmail.com>
2024-09-22 03:11:43 +0800

    added nfc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { execHaloCmdWeb } from '@arx-research/libhalo/api/web';
 
-import {
-  createAppKit,
-  useAppKit,
-  useAppKitEvents,
-  useAppKitState,
-  useAppKitTheme
-} from '@reown/appkit/react'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { createAppKit, useAppKit, useAppKitEvents, useAppKitState, useAppKitTheme } from '@reown/appkit/react';
+import { EthersAdapter } from '@reown/appkit-adapter-ethers';
+import { mainnet, arbitrum } from '@reown/appkit/networks';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // const projectId = import.meta.env.VITE_PROJECT_ID
 // if (!projectId) {
@@ -19,32 +16,53 @@ import { mainnet, arbitrum } from '@reown/appkit/networks'
 // }
 
 // 2. Set chains
-const networks = [mainnet, arbitrum]
+const networks = [mainnet, arbitrum];
 
-const ethersAdapter = new EthersAdapter()
+const ethersAdapter = new EthersAdapter();
 
 // 3. Create modal
-const projectId = "1"
+const projectId = '1';
 createAppKit({
   adapters: [ethersAdapter],
   networks,
   projectId,
   features: {
-    analytics: true
+    analytics: true,
   },
   themeMode: 'light',
   themeVariables: {
     '--w3m-color-mix': '#00DCFF',
-    '--w3m-color-mix-strength': 20
-  }
-})
+    '--w3m-color-mix-strength': 20,
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-  const modal = useAppKit()
-  const state = useAppKitState()
-  const { themeMode, themeVariables, setThemeMode } = useAppKitTheme()
-  const events = useAppKitEvents()
+  const [count, setCount] = useState(0);
+  const modal = useAppKit();
+  const state = useAppKitState();
+  const { themeMode, themeVariables, setThemeMode } = useAppKitTheme();
+  const events = useAppKitEvents();
+  const [statusText, setStatusText] = useState('Click on the button');
+
+  const btnClick = async () =>  {
+    let command = {
+      name: 'sign',
+      keyNo: 1,
+      message: '010203',
+    };
+
+    let res;
+
+    try {
+      // --- request NFC command execution ---
+      res = await execHaloCmdWeb(command);
+      // the command has succeeded, display the result to the user
+      setStatusText(JSON.stringify(res, null, 4));
+    } catch (e) {
+      // the command has failed, display error to the user
+      setStatusText('Error: ' + String(e));
+    }
+  }
 
   return (
     <>
@@ -55,16 +73,15 @@ function App() {
 
       <button onClick={() => modal.open()}>Connect Wallet</button>
       <button onClick={() => modal.open({ view: 'Networks' })}>Choose Network</button>
-      <button onClick={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}>
-        Toggle Theme Mode
-      </button>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-      <pre>{JSON.stringify({ themeMode, themeVariables }, null, 2)}</pre>
-      <pre>{JSON.stringify(events, null, 2)}</pre>
+      <button onClick={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}>Toggle Theme Mode</button>
+
+      <div>
+        <pre style={{ fontSize: 12, textAlign: 'left' }}>{statusText}</pre>
+        <button onClick={() => btnClick()}>Sign message 010203 using key #1</button>
+      </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
